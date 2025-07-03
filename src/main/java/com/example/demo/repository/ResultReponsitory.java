51a8c56d047ac5902267491c96d6dcb31d18ed8e
Mona Lisa <YOUR_EMAIL>
2025-07-03 15:25:10 +0700

    first commit

--- src/main/java/com/example/demo/repository/ResultReponsitory.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Result;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ResultReponsitory extends JpaRepository<Result,Long>{
-}
--- src/main/java/com/example/demo/repository/SubjectReponsitory.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectReponsitory extends JpaRepository<Subject,Long>{
-}
